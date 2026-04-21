c7b8a778c159a50efdff1464da29bd75b3150d22
AnisKamal <aniskamal52@gmail.com>
2026-04-21 01:04:43 +0100

    feat - ajout fetch query

---
+++ constants/useFetchQuery.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+
+const endpoint = "http://localhost:8099" 
+
+type API = {
+    'api/v1/matchs' : {
+        date: string, 
+        homeName: string,
+        homeLogo: string, 
+        awayName: string,
+        awayLogo: string
+    }
+}; 
+
+export function useFetchQuery<T extends keyof API>(
+  path: T, 
+  params?: Record<string, string | number>
+){
+    const localUrl = endpoint + Object.entries(params ?? {}).reduce(
+        (acc, [key, value]) => acc.replaceAll(`[${key}]`, String(value)), 
+        path as string
+    )
+    return useQuery({
+        queryKey: [localUrl],
+         queryFn: async () => {
+            return fetch(localUrl).then(r => r.json() as Promise<API[T]>)
+        }
+    })
+}
